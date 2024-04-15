9dfbac243648461e7e849121598d5ce9196dd7ec
devin.zhang <Z23@#>
2024-04-15 22:20:23 +0800

    submit

--- src/main/java/test/ticket/tickettools/domain/entity/UserInfoEntity.java
+++ src/main/java/test/ticket/tickettools/domain/entity/UserInfoEntity.java
@@ -14,11 +14,17 @@ public class UserInfoEntity {
     private String content;
     private Date createDate;
     private Date updateDate;
+    private String userName;
+    private String pwd;
+    private String nickName;
+    private String idCard;
+    private String idType;
+    private String headers;
 
     public UserInfoEntity() {
     }
 
-    public UserInfoEntity(Long id, String phoneNum, Integer channel, Long userId, String content, Date createDate, Date updateDate) {
+    public UserInfoEntity(Long id, String phoneNum, Integer channel, Long userId, String content, Date createDate, Date updateDate, String userName, String pwd, String nickName, String idCard, String idType, String headers) {
         this.id = id;
         this.phoneNum = phoneNum;
         this.channel = channel;
@@ -26,6 +32,12 @@ public class UserInfoEntity {
         this.content = content;
         this.createDate = createDate;
         this.updateDate = updateDate;
+        this.userName = userName;
+        this.pwd = pwd;
+        this.nickName = nickName;
+        this.idCard = idCard;
+        this.idType = idType;
+        this.headers = headers;
     }
 
     public Long getId() {
@@ -82,5 +94,53 @@ public class UserInfoEntity {
 
     public void setUpdateDate(Date updateDate) {
         this.updateDate = updateDate;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getIdType() {
+        return idType;
+    }
+
+    public void setIdType(String idType) {
+        this.idType = idType;
+    }
+
+    public String getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(String headers) {
+        this.headers = headers;
     }
 }
