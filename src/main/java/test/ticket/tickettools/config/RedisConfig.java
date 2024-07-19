5ce002f208217a40c951890bce86d4a837d1857f
devin.zhang <Z23@#>
2024-07-19 16:53:50 +0800

    7.19提交

--- src/main/java/test/ticket/tickettools/config/RedisConfig.java
+++ src/main/java/test/ticket/tickettools/config/RedisConfig.java
@@ -15,7 +15,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.util.ObjectUtils;
 
-@Configuration
+//@Configuration
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
