51132e745704aa4b34bc115a43fc42c84709d7d9
devin.zhang <Z23@#>
2024-07-20 20:32:06 +0800

    7.20提交

--- src/main/java/test/ticket/tickettools/TicketToolsApplication.java
+++ src/main/java/test/ticket/tickettools/TicketToolsApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication
 @MapperScan("test.ticket.tickettools.dao")
@@ -12,5 +15,14 @@ public class TicketToolsApplication {
     public static void main(String[] args) {
         SpringApplication.run(TicketToolsApplication.class, args);
     }
-
+    /**
+     * 因为定时器默认创建的线程池为一个线程的所以这里需要改成多个线程的，不然相同时间段任务执行时会等待,因为只有一个线程在工作
+     * @return
+     */
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(10);
+        return taskScheduler;
+    }
 }
