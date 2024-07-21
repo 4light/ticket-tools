12a70434ac34a7d84893f86dc70e946f8b1b41ba
devin.zhang <Z23@#>
2024-07-21 17:31:37 +0800

    7.20提交

--- src/main/java/test/ticket/tickettools/utils/ScreenshotUtil.java
+++ src/main/java/test/ticket/tickettools/utils/ScreenshotUtil.java
@@ -20,8 +20,8 @@ public class ScreenshotUtil {
     public static void takeScreenshot(String orderId,String auth,String fileName) {
         String url="https://pcticket.cstm.org.cn/personal/order_detail?orderId="+orderId;
         // 设置ChromeDriver的路径
-        System.setProperty("webdriver.chrome.driver", "/Users/devin.zhang/Downloads/chromedriver-mac-x64/chromedriver");
-        //System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver-linux64/chromedriver");
+        //System.setProperty("webdriver.chrome.driver", "/Users/devin.zhang/Downloads/chromedriver-mac-x64/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver-linux64/chromedriver");
 
         // 配置ChromeOptions
         ChromeOptions options = new ChromeOptions();
@@ -44,8 +44,8 @@ public class ScreenshotUtil {
             Thread.sleep(1000); // 可以使用更合适的等待方式，如WebDriverWait
             // 截图并保存
             File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            //String screenshotPath = "/root/screenShort/"+fileName+".png";
-            String screenshotPath = "./"+fileName+".png";
+            String screenshotPath = "/root/screenShort/"+fileName+".png";
+            //String screenshotPath = "./"+fileName+".png";
             File destination = new File(screenshotPath);
             FileUtil.copyFile(screenshot, destination);
             //String screenshotBase64 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
