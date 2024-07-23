1499e61cee0cdabffa1f70be444ea190e1fb8937
devin.zhang <Z23@#>
2024-07-23 08:47:24 +0800

    7.23提交

--- src/main/java/test/ticket/tickettools/schedule/DoSnatchingSchedule.java
+++ src/main/java/test/ticket/tickettools/schedule/DoSnatchingSchedule.java
@@ -216,6 +216,11 @@ public class DoSnatchingSchedule {
     }
     @Scheduled(fixedDelay = 60000)
     public void doUpdateAccountPool() {
+        try {
+            Thread.sleep(RandomUtil.randomInt(1000,10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         log.info("开始更新账号池");
         int accountNum=10;
         AccountInfoEntity query=new AccountInfoEntity();
