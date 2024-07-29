2d4da27bf45885e5bd38550974c8561e455805d5
devin.zhang <Z23@#>
2024-07-29 19:46:57 +0800

    7.25提交

--- src/main/java/test/ticket/tickettools/schedule/DoSnatchingSchedule.java
+++ src/main/java/test/ticket/tickettools/schedule/DoSnatchingSchedule.java
@@ -145,7 +145,7 @@ public class DoSnatchingSchedule {
         runNormalTask();
     }*/
 
-    @Scheduled(fixedDelay = 20000)
+    //@Scheduled(fixedDelay = 20000)
     public void updateOrderPayStatus() {
         try {
             RestTemplate restTemplate = TemplateUtil.initSSLTemplate();
@@ -211,7 +211,7 @@ public class DoSnatchingSchedule {
             }
         }
     }
-   @Scheduled(fixedDelay = 60000)
+   //@Scheduled(fixedDelay = 60000)
     public void doUpdateAccountPool() {
         /*LocalDateTime now=LocalDateTime.now();
         int hour = now.getHour();
