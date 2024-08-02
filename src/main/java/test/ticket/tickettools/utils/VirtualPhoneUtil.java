d798a526fb4624b43e569b2820b8e5e483a96372
devin.zhang <Z23@#>
2024-08-02 08:54:21 +0800

    7.25提交

--- src/main/java/test/ticket/tickettools/utils/VirtualPhoneUtil.java
+++ src/main/java/test/ticket/tickettools/utils/VirtualPhoneUtil.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 public class VirtualPhoneUtil {
     //获取号码
     private static String token = "12979f4ebab7cfff2eb0f62b76d68ff47604e99eabcb6f67dea8b3086c90b8ded10015176f6ac8131c921eed962625876572a81c2e15f0b4a7b497091f5fb39cc4b014bc4cf8ec85b53f4e5042c53ddd";
-    private static String getPhoneUrl = "https://api.haozhuma.com/sms/?api=getPhone&token=%s&sid=%s";
+    private static String getPhoneUrl = "https://api.haozhuma.com/sms/?api=getPhone&token=%s&sid=%s&ascription=2";
     //指定号码
     private static String designatedNumber="https://api.haozhuma.com/sms/?api=getPhone&token=%s&sid=%s&phone=%s";
     //获取验证码
